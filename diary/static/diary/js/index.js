a6bd5306a6b40e84045ff8acf5980b2c73046f78
Dmytro Kovalov <monkdok@gmail.com>
2020-06-06 22:17:29 +0300

    Workout ajax requests cleaned

--- diary/static/diary/js/index.js
+++ diary/static/diary/js/index.js
@@ -7,19 +7,11 @@ function appendToHtml(data) {
     $('body').removeClass('modal-open');
 }
 
-function updateWorkout(slug) {
-    if (slug) {
-        let workoutId = '#title' + slug
-        let title = $(workoutId).text()
-        $('#workout-update-title').val(title)
-        $('#workout-slug').val(slug)
-    }
-}
-
-function deleteWorkout(slug) {
-    $('#delete-workout').submit(function (e) {
+function deleteItem(slug) {
+    $('#delete-form').submit(function (e) {
         e.preventDefault()
-        let url = 'workout/' + slug + '/workout_delete'
+        // Pass view url through item attribute "data-url"
+        let url = $('#delete-dropdown' + slug).attr('data-url')
         let csrf_token = jQuery("[name=csrfmiddlewaretoken]").val()
 
         if (slug) {
@@ -42,42 +34,70 @@ function deleteWorkout(slug) {
 
 }
 
-// Update Django Ajax Call
-$("form#workout-update-form").submit(function(s) {
-    s.preventDefault()
-    let url = $(this).attr('action')
-    let workoutTitle = $('#workout-update-title').val()
-    let workoutSlug = $('#workout-slug').val()
-    let type = $(this).attr('type')
-    let url2 = 'workout/' + workoutSlug + '/update/'
+// Create Django Ajax Call
+$("#create-form").on('submit', function(e) {
+    e.preventDefault();
+    let inputTitle = $('input#input-title').val()
     let csrf_token = jQuery("[name=csrfmiddlewaretoken]").val()
-
-    console.log('after:', workoutTitle)
-    console.log('url:', url2)
-    console.log('this:', this)
-    console.log('slug:', workoutSlug)
     $.ajax({
-        url: url2,
-        data: {
-            slug: workoutSlug,
-            title: workoutTitle,
-            csrfmiddlewaretoken: csrf_token
-        },
-        type: 'post',
+        url: '/',
+        // data: form.serialize(),
+        data: {title: inputTitle, csrfmiddlewaretoken: csrf_token},
+        type: 'POST',
         dataType: 'json',
         success: function (data) {
             if (data.form_is_valid) {
-                $('.modal-backdrop').remove()
-                let html2 = document.querySelector('html')
-                $("body").html(data.html)
-                $('.dropdown-toggle').dropdown()
-                $('body').removeClass('modal-open')
-
+                appendToHtml(data)
             }
             else {
                 alert("All fields must have a valid value.")
-
             }
         }
-    });
+    })
+    // $("#workout-create-form").trigger("reset")
 })
+
+// Update Django Ajax Call
+function updateItem(slug) {
+    if (slug) {
+        // Passing initial form fields data
+        let workoutId = '#title' + slug
+        let title = $(workoutId).text()
+        $('#update-title').val(title)
+        $('#slug').val(slug)
+
+        // forming ajax request
+        $("form#update-form").submit(function(s) {
+            s.preventDefault()
+            let workoutTitle = $('#update-title').val()
+            let type = $(this).attr('type')
+            let url = $('#edit-dropdown' + slug).attr('data-url')
+            let csrf_token = jQuery("[name=csrfmiddlewaretoken]").val()
+
+            $.ajax({
+                url: url,
+                data: {
+                    slug: slug,
+                    title: workoutTitle,
+                    csrfmiddlewaretoken: csrf_token
+                },
+                type: 'post',
+                dataType: 'json',
+                success: function (data) {
+                    if (data.form_is_valid) {
+                        $('.modal-backdrop').remove()
+                        let html2 = document.querySelector('html')
+                        $("body").html(data.html)
+                        $('.dropdown-toggle').dropdown()
+                        $('body').removeClass('modal-open')
+
+                    }
+                    else {
+                        alert("All fields must have a valid value.")
+
+                    }
+                }
+            });
+        })
+    }
+}
